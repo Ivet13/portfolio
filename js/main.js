42c2fbeec57860b050cefeba6d471e5f6753c144
Ivet13 <jazzjackrabbit13@gmail.com>
2022-11-20 21:48:20 +0100

    copy to clipboard

--- js/main.js
+++ js/main.js
@@ -98,13 +98,10 @@ function myFunction() {
   // Get the text field
   var copyText = document.getElementById("myInput");
 
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
+  // Copy the text inside the text field
+  navigator.clipboard.writeText( copyText.innerHTML.value);
 
   // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+  alert("Copied the text: " +  copyText.innerHTML.value);
+  
 }
